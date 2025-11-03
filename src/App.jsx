e4511a95617e71946721154456e20bed6139f086
Juli Albrecht <juliella2002@gmail.com>
2025-11-03 16:29:43 +0100

    create new color card, connection componentes

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
 import "./App.css";
 import { ColorForm } from "../components/ColorForm";
+import { useState } from "react";
+import { uid } from "uid";
 
 function App() {
+  const [colors, setColors] = useState(initialColors);
+  function onSubmitColor(data) {
+    console.log(data);
+    const id = uid();
+    setColors([{ ...data, id }, ...colors]);
+  }
   return (
     <>
       <h1>Theme Creator</h1>
 
-      <ColorForm />
+      <ColorForm onSubmitColor={onSubmitColor} />
 
-      {initialColors.map((color) => {
+      {colors.map((color) => {
         return <Color key={color.id} color={color} />;
       })}
     </>
