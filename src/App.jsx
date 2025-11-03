fd8021185fd85fa6172bcb3ba565b6ed2359f19e
Juli Albrecht <juliella2002@gmail.com>
2025-11-03 11:40:02 +0100

    add component ColorForm which returns basic form with input-fields

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,26 @@ function App() {
     <>
       <h1>Theme Creator</h1>
 
+      <ColorForm />
+
       {initialColors.map((color) => {
         return <Color key={color.id} color={color} />;
       })}
     </>
+  );
+}
+
+function ColorForm() {
+  // const [input, setInput] = useState("");
+  return (
+    <form>
+      <label>Role</label>
+      <input type="text" placeholder="some color" />
+      <label>Hex</label>
+      <input type="text" placeholder="#123456" />
+      <label>Contrast Text</label>
+      <input type="text" placeholder="#ffffff" />
+    </form>
   );
 }
 
