7e8f36e0c2a1e70a7a97ea3ef9aaaa17997f743a
Juli Albrecht <juliella2002@gmail.com>
2025-11-04 12:42:39 +0100

    add DeleteButton component, add confirmation message, add message if no colors left in array

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,33 @@ import { uid } from "uid";
 
 function App() {
   const [colors, setColors] = useState(initialColors);
+
   function onSubmitColor(data) {
     console.log(data);
     const id = uid();
     setColors([{ ...data, id }, ...colors]);
   }
+
+  function deleteColor(colorID) {
+    const mutatedColors = colors.filter((color) => color.id !== colorID);
+    setColors(mutatedColors);
+  }
+
   return (
     <>
       <h1>Theme Creator</h1>
 
       <ColorForm onSubmitColor={onSubmitColor} />
 
-      {colors.map((color) => {
-        return <Color key={color.id} color={color} />;
-      })}
+      {colors.length === 0 ? (
+        <p>No colors... start by adding one!</p>
+      ) : (
+        colors.map((color) => {
+          return (
+            <Color key={color.id} color={color} deleteColor={deleteColor} />
+          );
+        })
+      )}
     </>
   );
 }
